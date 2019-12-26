4acf4986575d17b03ed8934b151a586292644971
pedroms9428 <pedro@Antergos-Dell>
2019-12-26 23:02:00 +0100

    Notebook añadido a las vistas y un campo precalculado

--- models/mecanico.py
+++ models/mecanico.py
@@ -5,12 +5,28 @@ Created on 29 nov. 2019
 '''
 from odoo import api, fields, models
 
+
 class mecanico(models.Model):
     _name = "upocar.mecanico"
     
-    nombre = fields.Char("Nombre", size=32, required = True)
-    apellidos = fields.Char("Apellidos", size=64, required = True)
-    especialidad = fields.Char("Especialidad", size=64, required = True)
+    _rec_name = "nombre_apellidos"
+    nombre = fields.Char("Nombre", size=32, required=True)
+    apellidos = fields.Char("Apellidos", size=64, required=True)
+    especialidad = fields.Char("Especialidad", size=64, required=True)
+    num_reparaciones = fields.Integer(string="Número de reparaciones", compute="_compute_num_reparaciones", store=True)
     
     reparacion_ids = fields.Many2many("upocar.reparacion", string="Reparaciones")
     taller_id = fields.Many2one("upocar.taller", string="Taller")
+    
+    nombre_apellidos = fields.Char(compute="_compute_nombre_apellidos")
+    
+    @api.depends("nombre", "apellidos")
+    def _compute_nombre_apellidos(self):
+        for record in self:
+            record.nombre_apellidos = record.nombre + " " + record.apellidos
+    
+    @api.depends("reparacion_ids")
+    def _compute_num_reparaciones(self):
+        for record in self:
+            if(record.reparacion_ids):
+                record.num_reparaciones = len(record.reparacion_ids)
