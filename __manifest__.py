478eaf12843973b5a80a3f77c69703ad0aa7b5a1
pedroms9428 <pedro@Antergos-Dell>
2020-01-04 00:19:17 +0100

    La factura se incluye en la reparación (factura eliminada).
    Actualización de vista y modelo de Reparación.
    Añadida comprobación que al terminar una reparación, sigue habiendo
    suficiente stock en el taller de los repuestos seleccionados.
    Al estar terminada o facturada una reparación, los campos Repuestos y
    Mecánicos se ponen readonly.

--- __manifest__.py
+++ __manifest__.py
@@ -7,8 +7,9 @@
     'category': 'UPOCAR',
     'version': '0.1',
     'depends': ['base'],
-    'data': ['views/reparacion_view.xml', 'views/vehiculo_view.xml', 'views/mecanico_view.xml', 'views/factura_view.xml',
-             'views/taller_view.xml', 'views/repuesto_view.xml', 'views/modelo_view.xml', 'views/marca_view.xml', 'views/linea_repuesto_view.xml', 'views/cliente_view.xml', 'views/proveedor_view.xml'],
+    'data': ['views/reparacion_view.xml', 'views/vehiculo_view.xml', 'views/mecanico_view.xml', 
+             'views/taller_view.xml', 'views/repuesto_view.xml', 'views/modelo_view.xml', 'views/marca_view.xml',
+             'views/linea_repuesto_view.xml', 'views/cliente_view.xml', 'views/proveedor_view.xml'],
     'demo': [],
     'application': True
 }
--- models/reparacion.py
+++ models/reparacion.py
@@ -12,7 +12,7 @@ class reparacion(models.Model):
     
     _rec_name = "descripcion"
     descripcion = fields.Char("Descripción de la reparacion", required=True, size=256)
-    fecha_inicio = fields.Datetime('Inicio de la reparación', required=True, autodate=True)
+    fecha_inicio = fields.Datetime('Inicio  de la reparación', required=True, autodate=True)
     fecha_fin = fields.Datetime('Fin de la reparación', autodate=True)
     numero_mecanicos = fields.Integer("Número de mecánicos", compute="_compute_numero_mecanicos", readonly=True, size=8, store=True)
     numero_dias = fields.Integer("Número de días en la reparación", compute="_compute_numero_dias", size=8, readonly=True, store=True)
@@ -20,15 +20,20 @@ class reparacion(models.Model):
     precio_hora = fields.Selection([(20, "20€"),
                                     (25, "25€"),
                                     (30, "30€"),
-                                    (35, "35€")], "Precio Hora")
+                                    (35, "35€")], "Precio Hora", default=20)
+    descuento = fields.Float("Descuento aplicado", (3, 2), required=True)
+    iva = fields.Selection([("0.1", '10%'),
+                          ("0.15", '15%'),
+                          ("0.21", '21%'), ],
+                          string='IVA', default='0.21', required=True)
+    importe_total = fields.Float("Importe total", (4, 2), compute="_get_importe_total", readonly=True, store=True)
     state = fields.Selection([('en_proceso', 'En proceso'),
                               ('terminada', 'Terminada'),
                               ('facturada', 'Facturada'), ], 'Estado', default='en_proceso')
     
     vehiculo_id = fields.Many2one("upocar.vehiculo", string="Vehículo reparado", required=True)
-    factura_id = fields.Many2one("upocar.factura", string="Factura")
     linea_repuesto_ids = fields.One2many("upocar.linea_repuesto", "reparacion_id", string="Repuestos utilizados")
-    mecanico_ids = fields.Many2many("upocar.mecanico", string="Mecánico")
+    mecanico_ids = fields.Many2many("upocar.mecanico", string="Mecánicos")
     taller_id = fields.Many2one("upocar.taller", string="Taller")
     
     @api.depends('fecha_inicio', 'fecha_fin')
@@ -46,10 +51,42 @@ class reparacion(models.Model):
         for record in self:
             if record.mecanico_ids:
                 record.numero_mecanicos = len(record.mecanico_ids)
+    
+    @api.depends('descuento', 'iva', 'linea_repuesto_ids', 'precio_hora')
+    def _get_importe_total(self):
+        for record in self:
+            if record.horas_trabajadas > 0 and record.numero_mecanicos > 0 and record.precio_hora != False:
+                record.importe_total = record.horas_trabajadas * record.numero_mecanicos * record.precio_hora
+            if len(record.linea_repuesto_ids) > 0:
+                for repuesto in record.linea_repuesto_ids:
+                    record.importe_total = record.importe_total + repuesto.repuesto_id.precio * repuesto.cantidad
+            record.importe_total -= record.importe_total * record.descuento / 100
+            record.importe_total += record.importe_total * float(record.iva)
+    
+    @api.onchange('fecha_fin','fecha_inicio')
+    def _check_date(self):
+        date_format = "%Y-%m-%d %H:%M:%S"
+        if self.fecha_inicio and self.fecha_fin:
+            dt1 = datetime.strptime(self.fecha_inicio, date_format)
+            dt2 = datetime.strptime(self.fecha_fin, date_format)
+            if dt2.__le__(dt1):
+                raise models.ValidationError("Error, la fecha fin debe ser posterior a la fecha inicio")
                 
+    @api.onchange('linea_repuesto_ids')
+    def _check_repetitions(self):
+        for linea_repuesto in self.linea_repuesto_ids:
+            count = 0
+            for linea_repuesto2 in self.linea_repuesto_ids:
+                if linea_repuesto.repuesto_id == linea_repuesto2.repuesto_id:
+                    count += 1
+            if count > 1:
+                raise models.ValidationError("Repuesto \"" + linea_repuesto.repuesto_id.nombre_repuesto + "\" repetido.")
+                break
+            
     @api.one
     def btn_submit_to_terminada(self):
         error = ""
+        repuesto = ""
         date_format = "%Y-%m-%d %H:%M:%S"
         if self.precio_hora == False:
             error += "Precio por hora incorrecto.\n"
@@ -63,15 +100,14 @@ class reparacion(models.Model):
         if self.horas_trabajadas == 0:
             error += "Horas de mano de obra debe ser mayor de 0.\n"
         if len(self.mecanico_ids) < 1:
-            error += "Ningún mecánico seleccionado."
-        for linea_repuesto in self.linea_repuesto_ids:
-            count = 0
-            for linea_repuesto2 in self.linea_repuesto_ids:
-                if linea_repuesto.repuesto_id == linea_repuesto2.repuesto_id:
-                    count += 1
-            if count >= 2:
-                error += "Repuesto repetido"
-                break
+            error += "Ningún mecánico seleccionado.\n"
+        if len(self.linea_repuesto_ids) > 0:
+            for linea_repuesto in self.linea_repuesto_ids:
+                if linea_repuesto.repuesto_id.cantidad < linea_repuesto.cantidad:
+                    repuesto = linea_repuesto.repuesto_id.nombre_repuesto
+                    break
+            if len(repuesto)>0:
+                error += "Stock insuficiente para el repuesto \"" + repuesto + "\", puede que se haya utilizado en otra reparación."
         if len(error) > 0:
             raise models.ValidationError("Error al terminar la reparación:\n" + error)
         else:
@@ -81,7 +117,4 @@ class reparacion(models.Model):
         
     @api.one
     def btn_submit_to_facturada(self):
-        if len(self.factura_id) > 0:
-            raise models.ValidationError("Error al facturar la reparación: No se ha seleccionado/creado ninguna factura")
-        else:
-            self.write({"state":"facturada"})
+        self.write({"state":"facturada"})
