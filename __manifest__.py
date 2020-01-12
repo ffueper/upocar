3191b5956def9251f8f90e6000822a2af3cab58e
usuario <fernandofuentesperez@gmail.com>
2020-01-12 14:01:11 +0100

    Agregado xml de datos demo

--- __manifest__.py
+++ __manifest__.py
@@ -11,6 +11,6 @@
              'views/taller_view.xml', 'views/repuesto_view.xml', 'views/modelo_view.xml', 'views/marca_view.xml',
              'views/linea_reparacion_view.xml', 'views/cliente_view.xml', 'views/proveedor_view.xml',
              'views/linea_pedido_view.xml', 'views/pedido_view.xml'],
-    'demo': [],
+    'demo': ['demo/upocar_demo.xml'],
     'application': True
 }
